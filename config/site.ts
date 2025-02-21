795b0c5d87bfe468dfb640b1fc78846840a22c7d
Jomo178 <shahbazi8@outlook.de>
2025-02-21 17:11:33 +0100

    feat: update site description and discord link in configuration

--- config/site.ts
+++ config/site.ts
@@ -2,11 +2,11 @@ import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
   name: "Tsuki",
-  description: `Join the fun with Tsuki Card Bot, featuring Jotsuki from SVT! Collect, trade, and discover stunning K-Pop cards in a minimal forest-themed gameplay. Enjoy gorgeous cards, cute roles, exclusive perks, giveaways (including Nitro), and a welcoming community. Perfect for K-Pop lovers and collectors!`,
+  description: `A kpop collecters card bot dedicated to Tsuki from Billlie.`,
   url: "https://tsuki-website.vercel.app/",
   ogImage: "https://tx.shadcn.com/og.jpg",
   links: {
-    discord: "https://discord.gg/THYH3czjDB",
+    discord: "https://discord.gg/H9EdBB6EY8",
     github: "https://github.com/Jomo178/tsuki-website",
   },
 };
